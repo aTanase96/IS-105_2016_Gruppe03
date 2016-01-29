0f0eaf31ce34194799640433f75f66fa89aa6c51
blackfang <blackfang101@hotmail.com>
2016-01-29 12:29:53 +0100

    .

---
+++ JokerPoker.py
@@ -0,0 +1,20 @@
+def main():
+    import random
+    
+    
+    cardValue = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+    cardColors = [hearts,diamonds,spades,clover]
+    cardsAll = []
+    
+    knight = int(11)
+    queen = int(12)
+    king = int(13)
+    ace = int(14)
+    
+    for x in cardValue
+    for y in cardColors
+    self.cardValue = rank
+    self.cardColors = suit
+    
+    main()
+    
