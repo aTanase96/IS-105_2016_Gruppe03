ccdbddc9d8c2453c041a0bd9409ec4c8e8a3126c
Martin Venaas <m@martinvenaas.com>
2016-02-10 08:52:45 +0100

    Revert "Oppdatert plusstest og lagt til unittest"
    This reverts commit d1ed837b391254b93bcff7cf93397bb3234e26f6.

--- plussfunksjonUnittest.py
+++
@@ -1,8 +0,0 @@
-import unittest # Importerer test bibliotek
-from plussfunksjon import plussFunksjon # Henter forst filen (plussfunksjon) og henter saa metoden (plussFunksjon)
-
-class plussTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(plussFunksjon(4,8), 12) # Skal sjekke hvorvidt resultatet stemmer 
-        
-raw_input() # Bruker maa trykke enter for aa lukke program
