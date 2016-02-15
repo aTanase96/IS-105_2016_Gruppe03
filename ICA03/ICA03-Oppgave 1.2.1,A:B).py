dac2b9bea04853941c62586a3efe9033326b416e
aTanase96 <itashi987@gmail.com>
2016-02-15 15:50:41 +0100

    Rydda opp litt :)

--- ICA03/ICA03-Oppgave 1.2.1,A:B).py
+++ ICA03/ICA03-Oppgave 1.2.1,A:B).py
@@ -3,14 +3,9 @@ def code():
     Implements an initial table for LZW algorithm 
     '''
     table = {}
-    table[1] = 'T'
-    table[2] = 'o'
-    table[3] = 'b'
-    table[4] = 'e'
-    table[5] = 'r'
-    table[6] = 'n'
-    table[7] = 't'
-    table[8] = ' '
+    table[1] = 'a'
+    table[2] = 'b'
+    table[3] = 'c'
     return table
 
 def encode(message):
@@ -36,7 +31,7 @@ def encode(message):
     
 
 def test():
-    test_message = "To be or not to be"
+    test_message = ""
     print encode(test_message)
     
 test()
