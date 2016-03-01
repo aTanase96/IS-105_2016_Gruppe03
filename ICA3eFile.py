39d7d5ec6acd430092edf0723fc9544927fd9c7a
blackfang <blackfang101@hotmail.com>
2016-03-01 14:47:47 +0100

    Rename testy.py to ICA3eFile.py

--- ICA3eFile.py
+++ ICA3eFile.py
@@ -3,8 +3,8 @@ def code(): #Lager en code funksjon skal code den gitte meldingen.
     Implements an initial table for LZW algorithm 
     '''
     table = {}             #Lager en tom table dictionary
-    table[1] = 'a'         #Legger inn noen tegn som skal være i dictionaryen
-    table[2] = 'b'         #Tablene auto-generere resten av tegnene til den har fått alle mulige varianter
+    table[1] = 'a'         #Legger inn noen tegn som skal vÃ¦re i dictionaryen
+    table[2] = 'b'         #Tablene auto-generere resten av tegnene til den har fÃ¥tt alle mulige varianter
     table[3] = 'c'
     table[4] = ' '
     return table
@@ -16,12 +16,12 @@ def encode(message):       #Encoder fuknsjonen som tar i bruk tabelen og funksjo
     for byte in message:    
         symbol = byte
         if (string + symbol) in table.values(): #Her finner koden stringen og symbolene i table verdiene
-            string = string + symbol        #Overskriver slik at stringen består av både symbol og string
+            string = string + symbol        #Overskriver slik at stringen bestÃ¥r av bÃ¥de symbol og string
         else:
             for k,v in table.iteritems():   #Her finner koden andre symboler som ikke ligger i tablen
                 if v == string:
                     code_for_string.append(k)
-            table[max(table.keys())+1] = string + symbol    #Øker lese "radius" med 1 plass for å få med andre symboler/string
+            table[max(table.keys())+1] = string + symbol    #Ã˜ker lese "radius" med 1 plass for Ã¥ fÃ¥ med andre symboler/string
             string = symbol
     for k,v in table.iteritems():
         if v == string:
