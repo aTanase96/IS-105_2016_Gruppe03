85a594527b70adf9689b84a9de763f125e76a1a3
blackfang <blackfang101@hotmail.com>
2016-03-12 19:41:15 +0100

    Uploading all rivertests rivertest not done

---
+++ rivertest.py
@@ -0,0 +1,9 @@
+import unittest
+import CrossingnewFinal
+class TestStringMethods(unittest.Testcase):
+    def test_mayhem(self):
+        self.assertTrue(mayhem()):
+        self.assertFalse():
+        
+if __name__ == '__main__':
+    unittest.main()
