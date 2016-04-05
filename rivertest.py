a1d36a1679a5022f879f10649ba39fc78c4b81af
blackfang <blackfang101@hotmail.com>
2016-04-06 01:08:32 +0200

    Removing all irrelevant stff

--- rivertest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import CrossingnewFinal
-class TestStringMethods(unittest.Testcase):
-    def test_mayhem(self):
-        self.assertTrue(mayhem()):
-        self.assertFalse():
-        
-if __name__ == '__main__':
-    unittest.main()
